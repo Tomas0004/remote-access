450bef40d0ffe9dc3d5b4423ccc8776ee2ecd2f6
Tomas0004 <peresprueba29@gmail.com>
2025-06-16 21:27:27 -0500

    feat: optimize the remote access

--- index.js
+++ index.js
@@ -1,47 +1,65 @@
-const express = require('express');
 const path = require('path');
 
-const PORT = 3000;
+const express = require('express');
+const { Server } = require('socket.io');
 const app = express();
+const httpServer = require('http').createServer(app);
+
+const io = new Server(httpServer);
+
+const PORT = 3000;
 
 const password = 'tomas1234';
 let status = { "status": 1 };
 
+io.on('connection', (socket) => {
+    console.log('Alguien se conecto al http');
+})
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'public', 'script')));
+// app.use(express.static(path.join(__dirname, 'public', 'script')));
 
 app.get('/', (req, res) => {
+    console.log('express: Alguien se conecto a la pagina');
     res.sendFile(path.join(__dirname, 'index.html'));
 })
 
-app.get('/status', (req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/event-stream',
-        'Cache-Control': 'no-cache',
-        'Connection': 'keep-alive',
-        'Access-Control-Allow-Origin': '*'
-    });
-    res.write('data: ' + JSON.stringify(status) + '\n\n');
+// app.get('/status', (req, res) => {
+//     res.writeHead(200, {
+//         'Content-Type': 'text/event-stream',
+//         'Cache-Control': 'no-cache',
+//         'Connection': 'keep-alive',
+//         'Access-Control-Allow-Origin': '*'
+//     });
+//     res.write('data: ' + JSON.stringify(status) + '\n\n');
 
-    res.status(200).send();
-})
+//     res.status(200).send();
+// })
 
 app.post('/status', (req, res) => {
     if (req.body.password == password) {
-        status.status = 2;
 
         res.redirect(req.body.redirect);
 
-        setTimeout(() => {
-            status.status = 1;
-        }, 70000)
-    }else{
+        status.status = 1;
+        let num = 0;
+
+        let interval = setInterval(() => {
+            num ++;
+            io.emit('message_server', status, num);
+
+            console.log(num);
+
+            if(num >= 70) interval.close();
+        }, 1000);
+
+    } else {
         res.redirect(req.body.redirect);
     }
 })
 
-app.listen(PORT, () => {
-    console.log(`Servidor levantado en http://localhost:${PORT}`);
+httpServer.listen(PORT, () => {
+    console.log(`Servidor http levantado en http://localhost:${PORT}`);
 })
